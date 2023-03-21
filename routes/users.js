9715d861c3eba87c45075cfd5edf7bcdc7b8b6ae
Ridho RP <ridhorachmatp@gmail.com>
2023-03-21 09:50:09 +0700

    api gateway register and login

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
-var express = require("express");
-var router = express.Router();
-const { APP_NAME } = process.env;
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send(APP_NAME);
-});
+const usersHandler = require("./handler/users");
+
+router.post("/register", usersHandler.register);
+router.post("/login", usersHandler.login);
 
 module.exports = router;
