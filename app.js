3f26ed32246126519a4f466f1631f54e26989f76
Ridho RP <ridhorachmatp@gmail.com>
2023-04-12 22:28:02 +0700

    cors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const cors = require("cors");
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const coursesRouter = require("./routes/courses");
@@ -23,6 +25,7 @@ const can = require("./middlewares/permission");
 
 const app = express();
 
+app.use(cors("*"));
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
